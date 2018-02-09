82783f9c513eccbd94f914b727526da58b8c3023
takeyan1004 <takeda.satoshi39@gmail.com>
2018-02-09 14:16:17 -0800

    updated envConfig

--- envConfig.sh
+++ envConfig.sh
@@ -10,13 +10,3 @@ PAGE_ACCESS_TOKEN=
 VERIFY_TOKEN=
 
 # note: .env is a shell file so there can't be spaces around '=
-
-
-#1. check if the page access token and verify token are correctly set to the env file and facebook app. 
-#2. it is better to create new facebook page. it is better to check facebook page, facebook app, and webhook code are all set up.
-#3.有効期限が原因で上手くセットアップ出来なかった可能性あり
-#4.facebook page→messenger プラットフォーム→リンクしたアプリで使用するfacebookアプリを紐づけてprimary receiverにする。他のアプリは設定しないことを推奨する。
-
-#after making the basic logic, option items, and structure, we need to implement 
-# nlp by using wit ai or dialogflow. We need to think about MECE and 
-#each business case. how to train ai for each case and how to reply to each case.
